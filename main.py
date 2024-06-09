2c2b1846fe33fe29096069b2a96ed971d19c2661
jared-strickland <strickland.jared@gmail.com>
2024-06-08 20:56:48 -0700

    Your commit message

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("hello world")
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+        words =file_contents.split()
+        num_words=len(words)
+    print (f"Number of words: {num_words}")
+
+if __name__ == "__main__":
+    main()
